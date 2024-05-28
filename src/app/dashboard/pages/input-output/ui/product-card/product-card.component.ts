bc53a99e8a6fda88bf99a4698ce0d5e3efd44086
Fernando Herrera <fernando.herrera85@gmail.com>
2024-05-28 10:32:27 -0400

    feat: Add product quantity increment functionality to product card component

--- src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
+++ src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
@@ -1,5 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  effect,
+  input,
+  output,
+} from '@angular/core';
+import { Product } from '@interfaces/product.interface';
 
 @Component({
   selector: 'app-product-card',
@@ -8,4 +18,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './product-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  // @Input({
+  //   required: true,
+  // })
+  // public product!: Product;
+  public product = input.required<Product>();
+
+  // @Output()
+  // public onIncrementQuantity = new EventEmitter<number>();
+  public onIncrementQuantity = output<number>();
+
+  public incrementQuantity(): void {
+    this.onIncrementQuantity.emit(this.product().quantity + 1);
+  }
+
+  public loginEffect = effect(() => {
+    console.log(this.product().name);
+  });
+}
