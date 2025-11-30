cdb4e2e7a17ae8e23e5a4fd5d069c746ca3b57bd
Esther Manrique <esthermanriqueepam@gmail.com>
2025-11-29 21:12:44 -0600

    Review-  heavy-loaders-slow component

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -9,21 +9,15 @@ import { CommonModule } from '@angular/common';
     <section [ngClass]="['w-full h-[600px]', cssClass]">
       Heavy Loader Slow
     </section>
-  `
+  `,
 })
 export class HeavyLoadersSlowComponent {
-
   @Input({ required: true }) cssClass!: string;
 
-
-
   constructor() {
-
     const start = Date.now();
-    while( Date.now() - start < 3000 ) {}
+    while (Date.now() - start < 3000) {}
 
     console.log('Cargado');
   }
-
-
 }
